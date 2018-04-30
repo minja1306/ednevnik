2b5b70f4139fbc585be66b38b97d45349e1a5831
Minja Slijepcevic <minja.slp@gmail.com>
2018-04-30 11:36:45 +0200

    e-dnevnik frontend deo

---
+++ src/app/evidencija.ts
@@ -0,0 +1,12 @@
+
+export class Evidencija {
+    ocenePrvo: number[];
+    zakljucnaPrvo: number;
+    oceneDrugo: number[];
+    zakljucnaDrugo: number;
+
+    constructor() {
+        this.ocenePrvo = [];
+        this.oceneDrugo = [];
+    }
+}
